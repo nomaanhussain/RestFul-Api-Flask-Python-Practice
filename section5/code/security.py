036fb5deea8a1fcac4ab9172820470debee94890
Noman Hussain <nomanghanchi_11@yahoo.com>
2018-12-24 01:06:03 +0500

    logging user

--- section5/code/security.py
+++ section5/code/security.py
@@ -1,24 +1,11 @@
 from werkzeug.security import safe_str_cmp
 from user import User
 
-
-users = [
-        User(1,'noman','asdf')
-]
-
-username_mapping = {
-    u.username: u for u in users
-}
-
-userid_mapping = {
-   u.id: u for u in users
-}
-
 def authenticate(username , password):
-    user = username_mapping.get(username, None)
+    user = User.find_by_username(username)
     if user and safe_str_cmp(user.password, password):
         return user
 
 def identity(payload):
     userid = payload['identity']
-    return userid_mapping.get(userid, None)
+    return User.find_by_id(userid)
