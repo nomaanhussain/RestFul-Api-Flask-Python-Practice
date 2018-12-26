6e52c75163f812939eb8cfb0dd4b94b5b23ec090
nomaanhussain <nomaanghanchi_11@yahoo.com>
2018-12-26 23:56:21 +0500

    section5 complete commit

--- section5/code/user.py
+++ section5/code/user.py
@@ -1,4 +1,5 @@
 import sqlite3
+from flask_restful import Resource, reqparse
 class User():
     def __init__(self, _id, username, password):
         self.id = _id
@@ -7,14 +8,14 @@ class User():
     
     @classmethod
     def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
+        connection = sqlite3.connect('database.db')
         cursor = connection.cursor()
 
         query = "SELECT * FROM users WHERE username=?"
         result = cursor.execute(query, (username,))
         row = result.fetchone()
         if row:
-            user = cls(*row)     # same as cls(row[0], row[1], row[2])
+            user = cls(*row)     # same as User(row[0], row[1], row[2])
         else:
             user = None
 
@@ -23,16 +24,52 @@ class User():
 
     @classmethod
     def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
+        connection = sqlite3.connect('database.db')
         cursor = connection.cursor()
 
         query = "SELECT * FROM users WHERE id=?"
         result = cursor.execute(query, (_id,))
         row = result.fetchone()
         if row:
-            user = cls(*row)
+            user = cls(*row)   # same as User(row[0], row[1], row[2])
         else:
             user = None
 
         connection.close()
         return user
+
+
+class UserRegister(Resource):
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+        type = str,
+        required=True,
+        help="this field cannot be left blank"
+    )
+    parser.add_argument('password',
+        type = str,
+        required=True,
+        help="this field cannot be left blank"
+    )
+
+    def post(self):
+        data = UserRegister.parser.parse_args()
+
+        if User.find_by_username(data["username"]):
+            return {"message": "user already exists with this username"}
+            
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"
+
+        
+        cursor.execute(query , (data["username"], data["password"]))
+
+        connection.commit()
+
+        connection.close()
+
+        return {"message": "user created successfully"}, 201
+
